530c19c7ca0a2d4ed7da20ce68d31f1340f5923e
Ruslan <Ruslan@Ruslan-PC>
2017-04-17 00:10:56 +0300

    The test

---
+++ src/main/java/com/sinsay/SinsayPaymentApplication.java
@@ -0,0 +1,12 @@
+package com.sinsay;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SinsayPaymentApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SinsayPaymentApplication.class, args);
+	}
+}
