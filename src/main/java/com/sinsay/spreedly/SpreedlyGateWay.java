11c72e7476bdb6347e6b4d43e8b84de80f7ebec8
r.poluektov <r.poluektov@itransition.com>
2017-04-17 18:33:12 +0300

    Add Classes

---
+++ src/main/java/com/sinsay/spreedly/SpreedlyGateWay.java
@@ -0,0 +1,25 @@
+package com.sinsay.spreedly;
+
+public class SpreedlyGateWay {
+    public Gateway gateway;
+
+    public SpreedlyGateWay(){
+    }
+
+    public SpreedlyGateWay(Gateway gateway){
+        this.gateway = gateway;
+    }
+
+    public static final class Gateway {
+
+    	public String gateway_type;
+
+        public Gateway(){
+        }
+
+
+        public Gateway(String gateway_type){
+            this.gateway_type = gateway_type;
+        }
+    }
+}
