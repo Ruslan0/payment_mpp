a97bfb6929d7da303098704b5f78acae68f0a785
r.poluektov <r.poluektov@itransition.com>
2017-04-17 19:17:30 +0300

    add payment, rename classes and packages

--- src/main/java/сom/customer/payment/service/SpreedlyService.java
+++ src/main/java/сom/customer/payment/service/SpreedlyService.java
@@ -15,20 +15,23 @@ public class SpreedlyService {
 	private  static final String plainCreds = "NMZajRzFonIo6tyliG3oU7viSTx:2zEEkrZ4nvWNpAn4BbJCHpjhgXJhUlqstRXdZl4l4Z4i4lWV0YLbejfvAWClKRdA";
 	private  static final String spreedly_url = "https://core.spreedly.com/v1/";
 	
-	public Object getGateWay(SpreedlyGateWay gatewayparam) {
-		
-		RestTemplate restTemplate = new RestTemplate();
+	private HttpHeaders getHeaders() {
 		byte[] plainCredsBytes = plainCreds.getBytes();
 		byte[] base64CredsBytes = Base64.encodeBase64(plainCredsBytes);
 		String base64Creds = new String(base64CredsBytes);		
-
-		String url = spreedly_url +"gateways.json";
+	
 		HttpHeaders headers = new HttpHeaders();
 		headers.setContentType(MediaType.APPLICATION_JSON);
 	    headers.add("Authorization", "Basic " + base64Creds);
+	    return headers;
+	}
+	
+	public Object getGateWay(SpreedlyGateWay gatewayparam) {
+		return new RestTemplate().postForObject(spreedly_url +"gateways.json", new HttpEntity<Object>(gatewayparam,getHeaders()), Object.class);
+	}
 
-		HttpEntity<?> entity = new HttpEntity<Object>(gatewayparam,headers);
-		return restTemplate.postForObject(url, entity, Object.class);
+	public Object payment(Object gatewayparam) {
+		return new RestTemplate().postForObject(spreedly_url +"gateways.json", new HttpEntity<Object>(gatewayparam,getHeaders()), Object.class);
 	}
 }
 
